ba29edbe8821ae887165da9d0aa0edc88ab2f035
Michele Sousa <21959@student.dorset-college.ie>
2021-01-25 13:30:49 +0000

    commit

---
+++ fewd_lab_2.js
@@ -0,0 +1,37 @@
+// 3. What is the length of the array?
+var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
+
+// 4. Write a function called myAlphabetLength which console.logs the length of the array
+
+//function myAlphabetLength()
+console.log(myAlphabet.length);
+
+
+// 5. Declare and initialize an array called 'Planets' with 5 string values.
+
+const Planets = ['Earth', 'Venus', 'Mars', 'Jupter', 'Saturn'];
+
+//6. Console.log each item using index in the array of Planets.
+console.log(Planets [0]);
+console.log(Planets [1]);
+console.log(Planets [2]);
+console.log(Planets [3]);
+console.log(Planets [4]);
+
+//Declare and initialize an array called wowDatatypes which has 3 different data types (NOT OBJECTS) and console.log the values.
+const wowDatatypes = ['']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
