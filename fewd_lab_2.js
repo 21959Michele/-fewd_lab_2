5daf79c91991991d7e24a7c789a71d0eeb0806e5
Michele Sousa <21959@student.dorset-college.ie>
2021-01-27 01:29:02 +0000

    commit

--- fewd_lab_2.js
+++ fewd_lab_2.js
@@ -1,11 +1,17 @@
+/*
+JS Lab 2
+Lecturer: Erika Volodko
+Student: Michele de Sousa Santos
+ID: 21959
+*/
+
+
 // 3. What is the length of the array?
 var myAlphabet = ['A', 'B', 'C', 'D','E','F', 'G'];
 
 // 4. Write a function called myAlphabetLength which console.logs the length of the array
-
 //function myAlphabetLength()
 console.log(myAlphabet.length);
-
 
 // 5. Declare and initialize an array called 'Planets' with 5 string values.
 
@@ -18,8 +24,35 @@ console.log(Planets [2]);
 console.log(Planets [3]);
 console.log(Planets [4]);
 
-//Declare and initialize an array called wowDatatypes which has 3 different data types (NOT OBJECTS) and console.log the values.
-const wowDatatypes = ['']
+
+//7. Declare and initialize an array called wowDatatypes which has 3 different data types (NOT OBJECTS) and console.log the values.
+const wowDatatypes = ['Michele', 30, 59.5];
+console.log("My name is  " + wowDatatypes[0] + ", I am " + wowDatatypes[1] + " years old and my current weight is " + wowDatatypes[2] + "kg");
+
+//Question 11
+// Declare a variable hello. Assign a function to it returning 'Hello world!'.
+ var hello = function(){
+     return 'Hello World!';
+     };
+     var result = hello();
+
+//12. Console.log the hello function.
+
+    console.log(result);
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
 
 
